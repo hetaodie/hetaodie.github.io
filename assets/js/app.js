8169da6f9ebcb90fd45239bc8abed7126a47d666
weixu <weixu6130@163.com>
2019-06-28 17:26:12 +0800

    修改重定向

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   alert("hello world");
-if(window.location.href=="http://hetaodie.github.io/")
+if(window.location.href=="http://hetaodie.github.io/" || window.location.href=="http://www.tianchilab.com")
 {
-$("#sidebar").css({width:'100%'});
+$("#sidebar").css({width:'10%'});
 $("#btnblog").click(function(){
 $("#sidebar").animate({width:'10.0%'},'slow');
 });
