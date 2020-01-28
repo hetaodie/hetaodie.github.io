01998ec96619502c29650464c58b82f7f9d8b869
weixu <weixu6130@163.com>
2020-01-28 14:34:37 +0800

    完成癌症预测部分

---
+++ assets/media/uda-ml/deep/JJWL/addsrt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+for (( i = 1; i < 20; i++ )); do
+	 ffmpeg -i ${i}.mp4 -vf subtitles=${i}-cn.srt:force_style='Alignment=2'  ${i}s.mp4
+	 ffmpeg -i ${i}s.mp4 -vf subtitles=${i}-en.srt:force_style='Alignment=6'  ${i}-t.mp4
+	 rm -f ${i}s.mp4
+	 rm -f ${i}.mp4
+done
